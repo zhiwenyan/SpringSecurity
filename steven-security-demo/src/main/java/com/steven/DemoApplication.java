c9bea9d635915a0edeabac12fa1c16383dfe1718
zhiwenyan <zhiwenyan@icloud.com>
2018-04-23 14:03:24 +0800

    test

--- steven-security-demo/src/main/java/com/steven/DemoApplication.java
+++ steven-security-demo/src/main/java/com/steven/DemoApplication.java
@@ -2,12 +2,10 @@ package com.steven;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
