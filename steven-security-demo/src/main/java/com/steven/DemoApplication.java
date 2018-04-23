8e083c359b7c5d35160ed47e72d30f3b33562621
zhiwenyan <zhiwenyan@icloud.com>
2018-04-23 13:47:41 +0800

    inital commit

---
+++ steven-security-demo/src/main/java/com/steven/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.steven;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+	private List<String> aa=new ArrayList<>();
+ 
+	private static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping("/hello")
+	private String hello(){
+		return "hello";
+	}
+	
+}
